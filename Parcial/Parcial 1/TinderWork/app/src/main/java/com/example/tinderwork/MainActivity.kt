ca7e486af7f5b79cd70cab98d837ddb37ebccc58
Carlos Guerrero <guerrerodavid42069@gmail.com>
2025-03-02 11:43:51 -0600

    Uploading Parcial 1

--- Parcial/Parcial 1/TinderWork/app/src/main/java/com/example/tinderwork/MainActivity.kt
+++ Parcial/Parcial 1/TinderWork/app/src/main/java/com/example/tinderwork/MainActivity.kt
@@ -26,12 +26,12 @@ class MainActivity : AppCompatActivity() {
         recyclerProfessionals.layoutManager = LinearLayoutManager(this)
 
         professionalList = listOf(
-            Professional("Lic. José Martínez", "Abogado", "10 años", R.drawable.abogado_jose, "jose@example.com", "77341732", "Especialista en derecho penal y derecho familiar, con experiencia en casos de alto perfil."),
-            Professional("Carlos Gómez", "Bombero", "7 años", R.drawable.bombero_carlos, "carlosbombero@example.com", "77341732", "Bombero con experiencia en rescate y atención de emergencias, comprometido con la seguridad comunitaria."),
-            Professional("Oficial Ana Rodríguez", "Policía", "6 años", R.drawable.policia_ana, "ana.policia@example.com", "77341732", "Policía con amplia experiencia en patrullaje, prevención de delitos y seguridad pública."),
-            Professional("Dr. Luis Fernández", "Médico General", "12 años", R.drawable.dr_luis, "luis@example.com", "77341732", "Médico general con experiencia en atención primaria, diagnóstico y tratamiento de enfermedades comunes."),
-            Professional("Arq. Gabriela Soto", "Arquitecta", "8 años", R.drawable.arq_gabriela, "gabriela@example.com", "77341732", "Arquitecta especializada en diseño urbano y proyectos residenciales, con enfoque en la sostenibilidad."),
-            Professional("Dra. Marta López", "Veterinaria", "4 años", R.drawable.dra_marta, "marta.veterinaria@example.com", "77341732", "Veterinaria con experiencia en atención clínica de mascotas y cirugías menores, apasionada por el cuidado animal.")
+            Professional("Lic. José Martínez", "Abogado", "10 años", R.drawable.abogado_jose, "jose@example.com", "00000000", "Especialista en derecho penal y derecho familiar, con experiencia en casos de alto perfil."),
+            Professional("Carlos Gómez", "Bombero", "7 años", R.drawable.bombero_carlos, "carlosbombero@example.com", "00000000", "Bombero con experiencia en rescate y atención de emergencias, comprometido con la seguridad comunitaria."),
+            Professional("Oficial Ana Rodríguez", "Policía", "6 años", R.drawable.policia_ana, "ana.policia@example.com", "00000000", "Policía con amplia experiencia en patrullaje, prevención de delitos y seguridad pública."),
+            Professional("Dr. Luis Fernández", "Médico General", "12 años", R.drawable.dr_luis, "luis@example.com", "00000000", "Médico general con experiencia en atención primaria, diagnóstico y tratamiento de enfermedades comunes."),
+            Professional("Arq. Gabriela Soto", "Arquitecta", "8 años", R.drawable.arq_gabriela, "gabriela@example.com", "00000000", "Arquitecta especializada en diseño urbano y proyectos residenciales, con enfoque en la sostenibilidad."),
+            Professional("Dra. Marta López", "Veterinaria", "4 años", R.drawable.dra_marta, "marta.veterinaria@example.com", "00000000", "Veterinaria con experiencia en atención clínica de mascotas y cirugías menores, apasionada por el cuidado animal.")
         )
 
         adapter = ProfessionalAdapter(this, professionalList)
